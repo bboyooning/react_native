3b8a0ab0ead44fa6af52e5c30b28863a3086ddd4
bboyooning <bboyooning@gmail.com>
2022-07-22 13:02:52 +0900

    add: [앱 화면 만들기] View, Text, ScrollView

--- rn_study/bbo/App.js
+++ rn_study/bbo/App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.textStyle}>안녕하세요, 보윤이에요!</Text>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  textContainer: {
+    height: 100,
+    borderColor: "#000",
+    borderWidth: 1,
+    borderRadius: 10,
+    margin: 10,
+  },
+  textStyle: {
+    textAlign: "center",
   },
 });
