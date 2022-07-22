00770283c6db8f6c62b06e86e6d2013553d12615
bboyooning <bboyooning@gmail.com>
2022-07-22 14:29:49 +0900

    add: data.json 생성 및 map 을 이용해 데이터 반복문 돌려보기

--- rn_study/bbo/App.js
+++ rn_study/bbo/App.js
@@ -7,13 +7,20 @@ import {
   TouchableOpacity,
   ScrollView,
 } from "react-native";
+import data from "./data.json";
 
 const main =
   "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fmain.png?alt=media&token=8e5eb78d-19ee-4359-9209-347d125b322c";
 export default function App() {
+  let tip = data.tip;
+  let todayWeather = 10 + 17;
+  let todayCondition = "흐림";
   return (
     <ScrollView style={styles.container}>
       <Text style={styles.title}>보윤이의 꿀팁</Text>
+      <Text style={styles.weather}>
+        오늘의 날씨: {todayWeather + "도 " + todayCondition}
+      </Text>
       <Image style={styles.mainImage} source={{ uri: main }} />
       <ScrollView
         style={styles.middleContainer}
@@ -34,24 +41,22 @@ export default function App() {
         </TouchableOpacity>
       </ScrollView>
       <View style={styles.cardContainer}>
-        <View style={styles.card}>
-          <Image
-            style={styles.cardImage}
-            source={{
-              uri: "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=1a099927-d818-45d4-b48a-7906fd0d2ad3",
-            }}
-          />
-          <View style={styles.cardText}>
-            <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게!</Text>
-            <Text style={styles.cardDesc} numberOfLines={3}>
-              먹다 남은 피자는 수분이 날라가기 때문에 처음처럼 맛있게 먹을 수
-              없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분
-              30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이
-              전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.
-            </Text>
-            <Text style={styles.cardDate}>2022.07.22</Text>
-          </View>
-        </View>
+        {tip.map((content, i) => {
+          return (
+            <View style={styles.card} key={i}>
+              <Image style={styles.cardImage} source={{ uri: content.image }} />
+              <View style={styles.cardText}>
+                <Text style={styles.cardTitle} numberOfLines={1}>
+                  {content.title}
+                </Text>
+                <Text style={styles.cardDesc} numberOfLines={3}>
+                  {content.desc}
+                </Text>
+                <Text style={styles.cardDate}>{content.date}</Text>
+              </View>
+            </View>
+          );
+        })}
       </View>
     </ScrollView>
   );
@@ -66,6 +71,10 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     marginTop: 60,
     marginLeft: 20,
+  },
+  weather: {
+    alignSelf: "flex-end",
+    paddingRight: 20,
   },
   mainImage: {
     width: "90%",
