4f48b4c629084be0a57ab068a625e1e2c0fa79e1
bboyooning <bboyooning@gmail.com>
2022-07-22 14:08:33 +0900

    modify: [앱 화면 만들기] 메인화면 꾸미기

--- rn_study/bbo/App.js
+++ rn_study/bbo/App.js
@@ -1,50 +1,154 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import {
-  Button,
-  ScrollView,
   StyleSheet,
   Text,
   View,
-  Alert,
-  TouchableOpacity,
   Image,
+  TouchableOpacity,
+  ScrollView,
 } from "react-native";
-import favicon from "./assets/favicon.png";
 
+const main =
+  "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fmain.png?alt=media&token=8e5eb78d-19ee-4359-9209-347d125b322c";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: "https://avatars.githubusercontent.com/u/77096364?v=4" }}
-        resizeMode={"cover"}
-        style={styles.imageStyle}
-      />
-    </View>
+    <ScrollView style={styles.container}>
+      <Text style={styles.title}>보윤이의 꿀팁</Text>
+      <Image style={styles.mainImage} source={{ uri: main }} />
+      <ScrollView
+        style={styles.middleContainer}
+        horizontal
+        indicatorStyle={"white"}
+      >
+        <TouchableOpacity style={styles.middleButton01}>
+          <Text style={styles.middleButtonText}>생활</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton02}>
+          <Text style={styles.middleButtonText}>재테크</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton03}>
+          <Text style={styles.middleButtonText}>반려견</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.middleButton04}>
+          <Text style={styles.middleButtonText}>꿀팁 찜</Text>
+        </TouchableOpacity>
+      </ScrollView>
+      <View style={styles.cardContainer}>
+        <View style={styles.card}>
+          <Image
+            style={styles.cardImage}
+            source={{
+              uri: "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Fpizza.png?alt=media&token=1a099927-d818-45d4-b48a-7906fd0d2ad3",
+            }}
+          />
+          <View style={styles.cardText}>
+            <Text style={styles.cardTitle}>먹다 남은 피자를 촉촉하게!</Text>
+            <Text style={styles.cardDesc} numberOfLines={3}>
+              먹다 남은 피자는 수분이 날라가기 때문에 처음처럼 맛있게 먹을 수
+              없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분
+              30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이
+              전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.
+            </Text>
+            <Text style={styles.cardDate}>2022.07.22</Text>
+          </View>
+        </View>
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: "#fff",
-    justifyContent: "center",
-    alignContent: "center",
   },
-  imageStyle: {
-    width: "100%",
-    height: "100%",
-    alignItems: "center",
-    justifyContent: "center",
+  title: {
+    fontSize: 20,
+    fontWeight: "700",
+    marginTop: 60,
+    marginLeft: 20,
   },
-  textContainer: {
-    height: 100,
-    borderColor: "#000",
-    borderWidth: 1,
+  mainImage: {
+    width: "90%",
+    height: 200,
     borderRadius: 10,
-    margin: 50,
+    marginTop: 20,
+    alignSelf: "center",
   },
-  textStyle: {
+  middleContainer: {
+    marginTop: 20,
+    marginLeft: 10,
+    height: 60,
+  },
+  middleButton01: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: "#fdc453",
+    borderColor: "deeppink",
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButton02: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: "#fe8d6f",
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButton03: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: "#9adbc5",
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButton04: {
+    width: 100,
+    height: 50,
+    padding: 15,
+    backgroundColor: "#f886a8",
+    borderRadius: 15,
+    margin: 7,
+  },
+  middleButtonText: {
+    color: "#fff",
+    fontWeight: "700",
     textAlign: "center",
+  },
+  cardContainer: {
+    marginTop: 10,
+    marginLeft: 10,
+  },
+  card: {
+    flex: 1,
+    flexDirection: "row",
+    margin: 10,
+    borderBottomWidth: 0.5,
+    borderBottomColor: "#eee",
+    paddingBottom: 10,
+  },
+  cardImage: {
+    flex: 1,
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  cardText: {
+    flex: 2,
+    flexDirection: "column",
+    marginLeft: 10,
+  },
+  cardTitle: {
+    fontSize: 20,
+    fontWeight: "700",
+  },
+  cardDesc: {
+    fontSize: 15,
+  },
+  cardDate: {
+    fontSize: 10,
+    color: "#A6A6A6",
   },
 });
