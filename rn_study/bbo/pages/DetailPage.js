b46c176367900e4923cee1932b4c081280c5eb1b
bboyooning <bboyooning@gmail.com>
2022-07-26 20:50:52 +0900

    add: react-native Share 을 이용한  페이지 내용 공유하기 기능 구현

--- rn_study/bbo/pages/DetailPage.js
+++ rn_study/bbo/pages/DetailPage.js
@@ -7,6 +7,7 @@ import {
   ScrollView,
   TouchableOpacity,
   Alert,
+  Share,
 } from "react-native";
 
 export default function DetailPage({ navigation, route }) {
@@ -35,15 +36,26 @@ export default function DetailPage({ navigation, route }) {
   const popup = () => {
     Alert.alert("꿀팁 찜!");
   };
+
+  const share = () => {
+    Share.share({
+      message: `${tip.title} \n\n ${tip.desc} \n\n ${tip.image}`,
+    });
+  };
   return (
     <ScrollView style={styles.container}>
       <Image style={styles.image} source={{ uri: tip.image }} />
       <View style={styles.textContainer}>
         <Text style={styles.title}>{tip.title}</Text>
         <Text style={styles.desc}>{tip.desc}</Text>
-        <TouchableOpacity style={styles.button} onPress={() => popup()}>
-          <Text style={styles.buttonText}>팁 찜하기</Text>
-        </TouchableOpacity>
+        <View style={styles.buttonGroup}>
+          <TouchableOpacity style={styles.button} onPress={() => popup()}>
+            <Text style={styles.buttonText}>팁 찜하기</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => share()}>
+            <Text style={styles.buttonText}>팁 공유하기</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </ScrollView>
   );
@@ -73,9 +85,14 @@ const styles = StyleSheet.create({
     marginTop: 10,
     color: "#eee",
   },
+  buttonGroup: {
+    flexDirection: "row",
+  },
   button: {
     width: 100,
     marginTop: 20,
+    marginRight: 10,
+    marginLeft: 10,
     padding: 10,
     borderWidth: 1,
     borderColor: "deeppink",
